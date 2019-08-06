dc59c9d62a9893db567dd2f0ab572910ed66f153
Henrik Aronsson Länsman <arnssn@gmail.com>
2019-08-06 20:10:44 +0200

    moving on

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,47 @@
 #include "libtcod.hpp"
-
 #include <iostream>
 
-int playerx=40,playery=25;
+// 
+// http://rogueliketutorials.com/tutorials/tcod/part-1/
+// 
+// http://www.roguebasin.com/index.php?title=Complete_roguelike_tutorial_using_C%2B%2B_and_libtcod_-_part_2:_map_and_actors
+// 
+// These differ somewhat
+
+const int SCREEN_WIDTH = 80;
+const int SCREEN_HEIGHT = 50;
+
+int playerx=SCREEN_WIDTH/2,playery=SCREEN_HEIGHT/2;
 
 int main( int argc, char *argv[] ) {
-   // TCODConsole::setCustomFont("data/arial10x10.png", TCOD_FONT_TYPE_GREYSCALE, TCOD_FONT_LAYOUT_TCOD);
-   TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);
-   TCOD_key_t key = {TCODK_NONE,0};
+    TCODConsole::setCustomFont("data/arial10x10.png", TCOD_FONT_TYPE_GREYSCALE | TCOD_FONT_LAYOUT_TCOD);
+    TCODConsole::initRoot(SCREEN_WIDTH, SCREEN_HEIGHT, "libtcod C++ tutorial", false);
+    TCOD_key_t key = {TCODK_NONE,0};
+     
+    auto root_console = TCODConsole::root;
 
-   while ( !TCODConsole::isWindowClosed() ) {
-       TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
-       switch(key.vk) {
-           case TCODK_UP : playery--; break;
-           case TCODK_DOWN : playery++; break;
-           case TCODK_LEFT : playerx--; break;
-           case TCODK_RIGHT : playerx++; break;
-           case TCODK_ESCAPE : return 0;
-           default:break;
-       }
-       TCODConsole::root->clear();
-       TCODConsole::root->putChar(playerx, playery, '@');
-       
-       TCODConsole::flush();
-   }
+    while ( !TCODConsole::isWindowClosed() ) {
+        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+        switch(key.vk) {
+            case TCODK_UP : playery--; break;
+            case TCODK_DOWN : playery++; break;
+            case TCODK_LEFT : playerx--; break;
+            case TCODK_RIGHT : playerx++; break;
+            case TCODK_ESCAPE : return 0;
+            case TCODK_ENTER: {
+                if(key.lalt) {
+                    TCODConsole::setFullscreen(!TCODConsole::isFullscreen());
+                }
+            }
+            default:break;
+        }
+        
+        root_console->setDefaultForeground(TCODColor::white);
+        root_console->clear();
+        root_console->putChar(playerx, playery, '@');
+        
+        TCODConsole::flush();
+    }
 
-   return 0;
+    return 0;
 }
